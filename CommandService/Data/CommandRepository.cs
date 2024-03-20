387fae385e3655ed6f7ccc13df1050ae7d3f91df
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2024-03-20 17:12:22 +0200

    Command Service

---
+++ CommandService/Data/CommandRepository.cs
@@ -0,0 +1,62 @@
+﻿using CommandService.Models;
+using System;
+using System.Linq;
+
+namespace CommandService.Data
+{
+    public class CommandRepository : ICommandRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CommandRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if(command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            _dbContext.Commands.Add(command);
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _dbContext.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Command> GetAllCommandsForPlatform(int platformId)
+        {
+            return _dbContext.Commands.Where(c => c.PlatformId == platformId);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _dbContext.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _dbContext.Commands.FirstOrDefault(c => c.PlatformId == platformId && c.Id == commandId);
+        }
+
+        public bool PlatformExists(int platformId)
+        {
+            return _dbContext.Platforms.Any(p => p.Id == platformId);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_dbContext.SaveChanges() >= 0);
+        }
+    }
+}
